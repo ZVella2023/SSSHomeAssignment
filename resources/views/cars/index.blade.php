e76bfbc3d92c65e0796adc18540819eb72b63258
Zachary <zacharyvella@gmail.com>
2023-12-21 16:58:42 +0000

    Saved table layout

---
+++ resources/views/cars/index.blade.php
@@ -0,0 +1,5 @@
+<h1>All Cars</h1>
+
+<a href="{{ route('cars.create') }}">Add New</a>
+<a href="{{ route('cars.show', 1) }}">Show Car</a>
+
---
+++ resources/views/cars/show.blade.php
@@ -0,0 +1,4 @@
+<h1>Car Details</h1>
+<div>{{ $car }}</div>
+
+<a href="{{ route('cars.index') }}">Back to all cars</a>
