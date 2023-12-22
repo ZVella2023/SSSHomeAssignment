b02c6d4199d5d3607f55ddc19670e91d6194a8f6
Zachary <zacharyvella@gmail.com>
2023-12-22 12:14:20 +0000

    Saved blade layout

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -1,5 +1,7 @@
-<h1>All Cars</h1>
+@extends('layouts.main')
 
+@section('content')
+<h1>All Cars</h1>
 <a href="{{ route('cars.create') }}">Add New</a>
 <a href="{{ route('cars.show', 1) }}">Show Car</a>
-
+@endsection
