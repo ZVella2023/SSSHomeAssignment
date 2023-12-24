81302827657616e8008025bd31654e0a68f76498
Zachary <zacharyvella@gmail.com>
2023-12-24 09:38:59 +0000

    Showing data of car models and manufacturers and filtering via dropdown menu

--- resources/views/cars/_filter.blade.php
+++ resources/views/cars/_filter.blade.php
@@ -4,10 +4,9 @@
             <div class="row">
                 <div class="col">
                     <div class="input-group mb-3">
-                        <select class="custom-select">
-                            <option value="" selected>All Manufacturers</option>
+                        <select id="filter_manufacturer_id" name="manufacturer_id" class="custom-select">
                             @foreach ($manufacturers as $id => $name)
-                                <option value="{{ $id }}">{{ $name }}</option>
+                                <option {{ $id == request('manufacturer_id') ? 'selected' : '' }} value="{{ $id }}">{{ $name }}</option>
                             @endforeach
                         </select>
                     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <div>
     <a href="{{ route('cars.index') }}">All Cars</a>
     <a href="{{ route('cars.create') }}">Add Car</a>
-    <a href="{{ route('cars.show', 1) }}">Show Cars id 1</a>
+    <a href="{{ route('cars.show', 1) }}">Show Car</a>
 </div>
